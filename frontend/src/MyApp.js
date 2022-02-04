81c943b9c0c852f78f9285fcd62f825ba7622efd
Joseph H <jhaluska@calpoly.edu>
2022-02-04 12:37:48 -0800

    added all files you will need to add .env and also make sure to do npm install in the directory to make sure node modules are installed

---
+++ frontend/src/MyApp.js
@@ -0,0 +1,97 @@
+import React, {useState, useEffect} from 'react';
+
+import Table from './Table';
+import Form from './Form';
+import axios from 'axios';
+
+
+
+
+function MyApp() {
+ 
+  const [characters, setCharacters] = useState([]);  
+
+  async function removeOneCharacter (index) {
+   const char = characters[index];
+   let mystr = 'http://localhost:5000/users/';
+   const updated = characters.filter((character, i) => {
+       return i !== index
+     });
+   mystr += char._id;
+   setCharacters(updated);
+   try {
+       const response = await axios.delete(mystr);
+       return response.data.users_list;     
+    }
+    catch (error){
+       //We're not handling errors. Just logging into the console.
+       console.log(error); 
+       return false;         
+    }
+
+        
+      }
+    
+
+      async function fetchAll(){
+        try {
+           const response = await axios.get('http://localhost:5000/users');
+           //console.log(response);
+           return response.data.users_list;     
+        }
+        catch (error){
+           //We're not handling errors. Just logging into the console.
+           console.log(error); 
+           return false;         
+        }
+     }
+
+     useEffect(() => {
+      fetchAll().then( result => {
+         if (result)
+            setCharacters(result);
+       });
+   }, [] );
+
+   
+   async function makePostCall(person){
+     try {
+       console.log("HEREEEE");
+       const response = await axios.post('http://localhost:5000/users', person);
+       //console.log(response.status)
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+    }
+
+
+    function updateList(person) { 
+      makePostCall(person).then( result => {
+      if (result && result.status === 201)
+      setCharacters([...characters, result.data] );
+      });
+      
+    }
+
+
+      return (
+        <div className="container">
+          <Table characterData={characters} removeCharacter={removeOneCharacter} />
+          <Form handleSubmit={updateList} />
+
+        </div>
+      )
+
+      
+      
+}
+
+ 
+
+
+ 
+
+export default MyApp;
