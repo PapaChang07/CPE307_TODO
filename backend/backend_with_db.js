6ba365621df026fa9237b59a6930097ee049e5dc
ChrisAruiza1015 <chrisaruiza57@gmail.com>
2022-02-16 13:47:31 -0800

    Added a second comment for CI

--- backend/backend_with_db.js
+++ backend/backend_with_db.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 // Test comment
+// Test comment # 2
 const userServices = require('./models/user-services');
 
 const app = express();
