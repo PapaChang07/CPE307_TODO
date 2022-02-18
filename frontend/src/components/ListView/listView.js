93823fc6d79313650e754c175d439a264b1e698d
Joseph H <jhaluska@calpoly.edu>
2022-02-18 14:25:25 -0800

    login and list view definitley work

--- frontend/src/components/ListView/listView.js
+++ frontend/src/components/ListView/listView.js
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import Todo from "../todo";
-import List from "./List";
-import Footer from "../Footer";
-import Header from "../Header";
-import { Row } from "react-bootstrap";
-import "./list.css";
-
-function ListView() {
-    const [input, setInput] = useState("");
-    const [todos, setTodos] = useState([]);
-
-    return (
-        <>
-            <Row>
-                <div className="header">
-                    <Header />
-                </div>
-            </Row>
-            <Row>
-                <div className="container">
-                    <div className="app-wrapper">
-                        <div>
-                            <List
-                                input={input}
-                                setInput={setInput}
-                                todos={todos}
-                                setTodos={setTodos}
-                            />
-                        </div>
-                        <div>
-                            <Todo todos={todos} setTodos={setTodos} />
-                        </div>
-                    </div>
-                </div>
-            </Row>
-            <Row>
-                <div className="footer-col col-md-4">
-                    <Footer />
-                </div>
-            </Row>
-        </>
-    )
-}
-
-=======
 import React, { useState } from "react";
 import Todo from "../todo";
 import List from "./List";
@@ -89,5 +42,4 @@ function ListView() {
     )
 }
 
->>>>>>> 07625e40ed50ce4b4a436eb47204b059b47d3abd
 export default ListView;
--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,67 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { Container, Form, Button, Row } from 'react-bootstrap';
-import { useNavigate } from "react-router-dom";
-import Footer from "../Footer";
-import "./Login.css";
-// import { useForm } from "react-hook-form";
-// import { v4 as uuidv4 } from 'uuid';
-// import axios from 'axios';
-
-// function submitForm(props) {
-//   props.handleSubmit(person);
-//   setPerson({username: '', password: ''});
-// }
-
-function Login() {
-  const [user, setUser] = useState("");
-  const [password, setPassword] = useState("");
-
-  const navigate = useNavigate();
-  const handleSubmit = () => {
-    console.log("listview");
-    navigate('/listview');
-  }
-
-  return (
-    <>
-      <div className="header"></div>
-      <Row className="justify-content-md-center">
-        <Container sm="fluid" className='login-form'>
-          <div className="app-wrapper">
-            <h1 className="login-header">Login</h1>
-            <form id="login" onSubmit={handleSubmit}>
-              <Form.Group controlId="username">
-                <Form.Control
-                  type="text"
-                  placeholder="Username..."
-                  className="username"
-                  value={user}
-                  required
-                  onChange={(e) => setUser(e.target.value)}
-                />
-              </Form.Group>
-              <Form.Group controlId="password">
-                <Form.Control
-                  type="password"
-                  placeholder="Password..."
-                  className="username"
-                  value={password}
-                  required
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </Form.Group>
-              <Button className='button-add' type="submit">Login</Button>
-            </form>
-          </div>
-        </Container>
-      </Row>
-      <Footer></Footer>
-    </>
-  );
-}
-
-=======
 import React, { useState } from 'react';
 import { Container, Form, Button, Row } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
@@ -123,5 +59,4 @@ function Login() {
   );
 }
 
->>>>>>> 07625e40ed50ce4b4a436eb47204b059b47d3abd
 export default Login;
