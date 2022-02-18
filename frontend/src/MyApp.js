36b6a0804014b69974e57795b483ed38784728bb
Shad <shad12@outlook.com>
2022-02-18 14:02:24 -0800

    pushing to list-view

--- frontend/src/MyApp.js
+++ frontend/src/MyApp.js
@@ -1,12 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import ListView from "./components/ListView/listView";
-import Login from "./components/Login/Login";
-
-function MyApp() {
-
-=======
 import ListView from "./components/ListView/listView";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
@@ -14,7 +6,6 @@ import "./index.css";
 
 function MyApp() {
   
->>>>>>> 07625e40ed50ce4b4a436eb47204b059b47d3abd
   return (
     <Router>
       <Routes>
