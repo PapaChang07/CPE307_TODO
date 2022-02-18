3ea32ce2535830e88b12300f1f21f94e16a21403
Shad <shad12@outlook.com>
2022-02-18 13:00:22 -0800

    linking works

--- frontend/src/MyApp.js
+++ frontend/src/MyApp.js
@@ -1,45 +1,18 @@
-import React, { useState } from "react";
-import Header from "./components/Header";
-import List from "./components/List";
-import Todo from "./components/todo";
-import Footer from "./components/Footer";
-import { Row } from "react-bootstrap";
+import React from "react";
+import ListView from "./components/ListView/listView";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
 import "./index.css";
 
 function MyApp() {
-  const [input, setInput] = useState("");
-  const [todos, setTodos] = useState([]);
-
+  
   return (
-    <>
-      <Row>
-        <div className="header">
-          <Header/>
-        </div>
-      </Row>
-      <Row>
-        <div className="container">
-          <div className="app-wrapper">
-            <div>
-              <List
-                input={input}
-                setInput={setInput}
-                todos={todos}
-                setTodos={setTodos}
-              />
-            </div>
-            <div>
-              <Todo todos={todos} setTodos={setTodos} />
-            </div>
-          </div>
-        </div>
-      </Row>
-      <Row>
-        <div className="footer-col col-md-4">
-          <Footer/>
-        </div>
-      </Row>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+        <Route path="/listview" element={<ListView />}/>
+      </Routes>
+    </Router>
   )
 }
 
--- frontend/src/components/ListView/List.js
+++ frontend/src/components/ListView/List.js
@@ -1,13 +1,10 @@
-import React, {useState, useEffect} from 'react';
-
+import React, { useState, useEffect } from 'react';
 import { Form, Button } from 'react-bootstrap';
-//import { useForm } from "react-hook-form";
 import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios';
 
-
 const List = ({ input, setInput, todos, setTodos }) => {
-    const [mytasks, setTasks] = useState([]);  
+    const [mytasks, setTasks] = useState([]);
 
     const onInputChange = (event) => {
         setInput(event.target.value);
@@ -19,35 +16,30 @@ const List = ({ input, setInput, todos, setTodos }) => {
         setInput("");
     };
 
-    async function fetchAll(){
+    async function fetchAll() {
         try {
-           const response = await axios.get('http://localhost:5000/users');
-           //console.log(response);
-           return response.data.users_list;     
+            const response = await axios.get('http://localhost:5000/users');
+            //console.log(response);
+            return response.data.users_list;
         }
-        catch (error){
-           //We're not handling errors. Just logging into the console.
-           console.log(error); 
-           return false;         
+        catch (error) {
+            //We're not handling errors. Just logging into the console.
+            console.log(error);
+            return false;
         }
-     }
+    }
     useEffect(() => {
-        fetchAll().then( result => {
-           if (result){
-            setTasks(result);
-           }
-              
-         });
-     }, [] );
-     
+        fetchAll().then(result => {
+            if (result) {
+                setTasks(result);
+            }
 
-    //const { handleSubmit } = useForm();
-    
-    
+        });
+    }, []);
+
     let taskItems = [];
 
-    if(mytasks.length !== 0){
- 
+    if (mytasks.length !== 0) {
         let user = mytasks.find(person => person.name === "Eric");
         taskItems = user.tasks.map((task) => <li className='list'>{task.body}</li>);
     }
