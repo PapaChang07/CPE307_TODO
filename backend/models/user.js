cf97ec33df40b10981efceb0c7f700a24afa4e82
Joseph H <jhaluska@calpoly.edu>
2022-02-18 12:44:29 -0800

    prettier fix for user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,32 +1,32 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  password: {
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
       required: true,
-      trim: true,
+      trim: true
     },
-  tasks: {
-    type: Array, 
-    required: false
+    password: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    tasks: {
+      type: Array,
+      required: false
+    }
   },
-}, {collection : 'users_list'});
+  { collection: "users_list" }
+);
 
 const User = mongoose.model("User", UserSchema);
-
 
 function Task(imp, flag, body, date) {
   this.imp = imp;
   this.flag = flag;
   this.body = body;
   this.date = date;
-
 }
-
 
 module.exports = User;
