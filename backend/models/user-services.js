2875bbc9babd341dbd0c485faa6f48e59a12d322
Joseph H <jhaluska@calpoly.edu>
2022-02-18 12:57:29 -0800

    fixed prettier checks hopefully

--- backend/models/user-services.js
+++ backend/models/user-services.js
@@ -1,10 +1,7 @@
-const res = require('express/lib/response');
-const mongoose = require('mongoose');
+//const res = require("express/lib/response");
+const mongoose = require("mongoose");
 const userModel = require("./user");
 const dotenv = require("dotenv");
-
-
-
 
 dotenv.config();
 
@@ -28,67 +25,59 @@ mongoose
   )
   .catch((error) => console.log(error));
 
-  
-
-async function getUsers(name, password){
-    let result;
-    if (name === undefined && password === undefined){
-        result = await userModel.find();
-    }
-    else if (name && !password) {
-        result = await findUserByName(name);
-    }
-    else if (password && !name){
-        result = await findUserByJob(password);
-    }   
-    else if(password && name){
-        result = await findUserByNameandJob(name, password);
-    }
-    return result;  
+async function getUsers(name, password) {
+  let result;
+  if (name === undefined && password === undefined) {
+    result = await userModel.find();
+  } else if (name && !password) {
+    result = await findUserByName(name);
+  } else if (password && !name) {
+    result = await findUserByJob(password);
+  } else if (password && name) {
+    result = await findUserByNameandJob(name, password);
+  }
+  return result;
 }
 
-async function findUserById(id){
-    try{
-        return await userModel.findById(id);
-    }catch(error) {
-        console.log(error);
-        return undefined;
-    }
+async function findUserById(id) {
+  try {
+    return await userModel.findById(id);
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
 }
 
-async function addUser(user){
-    try{
-        const userToAdd = new userModel(user);
-        const savedUser = await userToAdd.save()
-        return savedUser;
-    }catch(error) {
-        console.log(error);
-        return false;
-    }   
+async function addUser(user) {
+  try {
+    const userToAdd = new userModel(user);
+    const savedUser = await userToAdd.save();
+    return savedUser;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
-async function deleteByID(id){
-     try{
-         return await userModel.findByIdAndDelete(id);
-    }
-    catch(error){
-         return false;
-    }
+async function deleteByID(id) {
+  try {
+    return await userModel.findByIdAndDelete(id);
+  } catch (error) {
+    return false;
+  }
 }
 
-async function findUserByName(name){
-    return await userModel.find({'name':name});
+async function findUserByName(name) {
+  return await userModel.find({ name: name });
 }
 
-async function findUserByJob(password){
-    return await userModel.find({'password':password});
+async function findUserByJob(password) {
+  return await userModel.find({ password: password });
 }
 
-async function findUserByNameandJob(name, job){
-    return await userModel.find({'password':password, 'name': name});
+async function findUserByNameandJob(name, password) {
+  return await userModel.find({ password: password, name: name });
 }
-
-
 
 exports.getUsers = getUsers;
 exports.findUserById = findUserById;
