d1bc61883d923cc2f36a929411e0df253d522df8
Joseph H <jhaluska@calpoly.edu>
2022-02-18 12:50:31 -0800

    fixeduser.js style

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,28 +5,21 @@ const UserSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
     },
     password: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
     },
     tasks: {
       type: Array,
-      required: false
-    }
+      required: false,
+    },
   },
   { collection: "users_list" }
 );
 
 const User = mongoose.model("User", UserSchema);
-
-function Task(imp, flag, body, date) {
-  this.imp = imp;
-  this.flag = flag;
-  this.body = body;
-  this.date = date;
-}
 
 module.exports = User;
