f0c43c2c5bf134560a291fa930fccc0963c778ba
Joseph H <jhaluska@calpoly.edu>
2022-03-04 12:59:13 -0800

    delete is working

--- frontend/src/components/ListView/listView.js
+++ frontend/src/components/ListView/listView.js
@@ -4,16 +4,65 @@ import List from "./List";
 import Footer from "../Footer";
 import Header from "../Header";
 import { Row } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 import "./list.css";
 
 function ListView() {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
   const [editTodo, setEditTodo] = useState(null);
+  const [user, setUser] = useState("");
+  const [myTasks, setTasks] = useState([]);
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
+
+  async function fetchAll() {
+    try {
+      //const response = await axios.get("https://cpe307-todo-backend.herokuapp.com/users");
+      const response = await axios.get("http://localhost:5000/users");
+      //console.log(response);
+      return response.data.users_list;
+    } catch (error) {
+      //We're not handling errors. Just logging into the console.
+      console.log(error);
+      return false;
+    }
+  }
+  useEffect(() => {
+    fetchAll().then((result) => {
+      if (result) {
+        let tasks = [];
+
+        let user = result.find((person) => person.name === "Juan");
+        console.log(user);
+        let items = user.tasks.map((task) => (
+          <li className="list">{task.body}</li>
+        ));
+        setTasks(items);
+        console.log(myTasks);
+        setUser(user);
+        for (let x in user.tasks) {
+          console.log(x);
+
+          setInput(user.tasks[x].body);
+          setTodos([
+            ...tasks,
+            { id: uuidv4(), title: user.tasks[x].body, completed: user.tasks.flag },
+          ]);
+          tasks.push({
+            id: uuidv4(),
+            title: user.tasks[x].body,
+            completed: user.tasks.flag,
+          });
+          setInput("");
+        }
+      }
+    });
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
@@ -31,6 +80,10 @@ function ListView() {
                 setTodos={setTodos}
                 editTodo={editTodo}
                 setEditTodo={setEditTodo}
+                user = {user}
+                setUser = {setUser}
+                myTasks = {myTasks}
+                setTasks = {setTasks}
               />
             </div>
             <div>
@@ -38,6 +91,9 @@ function ListView() {
                 todos={todos}
                 setTodos={setTodos}
                 setEditTodo={setEditTodo}
+                user ={user}
+                setUser = {setUser}
+
               />
             </div>
           </div>
